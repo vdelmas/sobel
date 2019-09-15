c350d59480ad73b9a96fcf24bac9030b16ba3f63
Vincent Delmas <vincent.delmas@hotmail.fr>
2019-09-14 22:22:37 -0400

    rectification en fait ça marchait plus si y avait un path mais la c'est bon normalement

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sys
 
 #Loading image and converting to "L" mode (greyscale)
 path, name = os.path.split(sys.argv[1])
-path = os.getcwd() + path
+path = os.getcwd() + os.sep + path
 bare_name = name.split(".")[0]
 im = Image.open(sys.argv[1]).convert("L")
 
